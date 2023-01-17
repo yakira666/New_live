71dca680990cc761e9e9ae71ecf4a9c08ddb6b32
Kirill <zebzeeff.kirill@yandex.ru>
2023-01-17 20:59:02 +0300

    Решение задач с кодварс + решение

--- codewars_py/sum_of_digit.py
+++ codewars_py/sum_of_digit.py
@@ -1,8 +1,20 @@
+# def digital_root(n):
+#     s = sum([int(el) for el in str(n)])
+#     if len(str(s)) == 1:
+#         return s
+#     return digital_root(s)
+
 def digital_root(n):
-    s = sum([int(el) for el in str(n)])
-    if len(str(s)) == 1:
-        return s
-    return digital_root(s)
+    total = 0
+
+    while n > 0:
+        total += n % 10
+        n //= 10
+
+    if total < 10:  # Recursion exit condition
+        return total
+
+    return digital_root(total)  # recursive call
 
 
 if __name__ == '__main__':
