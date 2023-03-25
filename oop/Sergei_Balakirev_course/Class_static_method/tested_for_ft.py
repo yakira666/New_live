729f09b66bd6c25211218d3a5bb80bde5f255d2b
Kirill <zebzeeff.kirill@yandex.ru>
2023-03-25 21:57:55 +0300

    Окончательный тест первого модуля

---
+++ oop/Sergei_Balakirev_course/Class_static_method/tested_for_ft.py
@@ -0,0 +1,17 @@
+from first_test import *
+
+router = Router()
+sv_from = Server()
+sv_from2 = Server()
+router.link(sv_from)
+router.link(sv_from2)
+router.link(Server())
+router.link(Server())
+sv_to = Server()
+router.link(sv_to)
+sv_from.send_data(Data("Hello", sv_to.get_ip()))
+sv_from2.send_data(Data("Hello", sv_to.get_ip()))
+sv_to.send_data(Data("Hi", sv_from.get_ip()))
+router.send_data()
+msg_lst_from = sv_from.get_data()
+msg_lst_to = sv_to.get_data()
